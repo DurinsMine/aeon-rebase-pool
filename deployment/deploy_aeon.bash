7c4ca27baa17a7edb92034c95ef23626cbca06eb
DurinsMine <40305022+DurinsMine@users.noreply.github.com>
2018-06-16 14:17:47 +0100

    Update for durinscoin

--- deployment/deploy_aeon.bash
+++ deployment/deploy_aeon.bash
@@ -16,7 +16,7 @@ sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again p
 echo -e "[client]\nuser=root\npassword=$ROOT_SQL_PASS" | sudo tee /root/.my.cnf
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y install git python-virtualenv python3-virtualenv curl ntp build-essential screen cmake pkg-config libboost-all-dev libevent-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev libgtest-dev mysql-server lmdb-utils libzmq3-dev doxygen graphviz libsodium-dev
 cd ~
-git clone https://github.com/ArqTras/aeon-rebase-pool.git  # Change this depending on how the deployment goes.
+git clone https://github.com/DurinsMine/durins-nodejs-pool.git  # Change this depending on how the deployment goes.
 cd /usr/src/gtest
 sudo cmake .
 sudo make
@@ -24,25 +24,25 @@ sudo mv libg* /usr/lib/
 cd ~
 sudo systemctl enable ntp
 cd /usr/local/src
-sudo git clone -b aeon-rebase-new https://github.com/stoffu/monero.git aeon
-cd aeon
+sudo git clone -b aeon-rebase-new https://github.com/durinsmine/durinscoin.git durins
+cd durins
 sudo make -j$(nproc)
-sudo cp ~/nodejs-pool/deployment/aeon.service /lib/systemd/system/
-sudo useradd -m aeondaemon -d /home/aeondaemon
+sudo cp ~/durins-nodejs-pool/deployment/durins.service /lib/systemd/system/
+sudo useradd -m durinsdaemon -d /home/durinsdaemon
 sudo systemctl daemon-reload
-sudo systemctl enable aeon
-sudo systemctl start aeon
+sudo systemctl enable durins
+sudo systemctl start durins
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
 source ~/.nvm/nvm.sh
 nvm install v8.9.3
-cd ~/aeon-rebase-pool
+cd ~/durins-nodejs-pool
 npm install
 npm install -g pm2
 openssl req -subj "/C=IT/ST=Pool/L=Daemon/O=Mining Pool/CN=mining.pool" -newkey rsa:2048 -nodes -keyout cert.key -x509 -out cert.pem -days 36500
 mkdir ~/pool_db/
 sed -r "s/(\"db_storage_path\": ).*/\1\"\/home\/$CURUSER\/pool_db\/\",/" config_example.json > config.json
 cd ~
-git clone https://github.com/ArqTras/poolui.git
+git clone https://github.com/durinsmine/poolui.git
 cd poolui
 npm install
 ./node_modules/bower/bin/bower update
@@ -63,7 +63,7 @@ sudo chown -R root:www-data /etc/caddy
 sudo mkdir /etc/ssl/caddy
 sudo chown -R www-data:root /etc/ssl/caddy
 sudo chmod 0770 /etc/ssl/caddy
-sudo cp ~/aeon-rebase-pool/deployment/caddyfile /etc/caddy/Caddyfile
+sudo cp ~/durins-nodejs-pool/deployment/caddyfile /etc/caddy/Caddyfile
 sudo chown www-data:www-data /etc/caddy/Caddyfile
 sudo chmod 444 /etc/caddy/Caddyfile
 sudo sh -c "sed 's/ProtectHome=true/ProtectHome=false/' init/linux-systemd/caddy.service > /etc/systemd/system/caddy.service"
@@ -75,15 +75,15 @@ sudo systemctl start caddy.service
 rm -rf $CADDY_DOWNLOAD_DIR
 cd ~
 sudo env PATH=$PATH:`pwd`/.nvm/versions/node/v8.9.3/bin `pwd`/.nvm/versions/node/v8.9.3/lib/node_modules/pm2/bin/pm2 startup systemd -u $CURUSER --hp `pwd`
-cd ~/aeon-rebase-pool
+cd ~/durins-nodejs-pool
 sudo chown -R $CURUSER. ~/.pm2
 echo "Installing pm2-logrotate in the background!"
 pm2 install pm2-logrotate &
-mysql -u root --password=$ROOT_SQL_PASS < deployment/baseaeon.sql
+mysql -u root --password=$ROOT_SQL_PASS < deployment/basedurins.sql
 mysql -u root --password=$ROOT_SQL_PASS pool -e "INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('api', 'authKey', '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`', 'string', 'Auth key sent with all Websocket frames for validation.')"
 mysql -u root --password=$ROOT_SQL_PASS pool -e "INSERT INTO pool.config (module, item, item_value, item_type, Item_desc) VALUES ('api', 'secKey', '`cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1`', 'string', 'HMAC key for Passwords.  JWT Secret Key.  Changing this will invalidate all current logins.')"
 pm2 start init.js --name=api --log-date-format="YYYY-MM-DD HH:mm Z" -- --module=api
-bash ~/aeon-rebase-pool/deployment/install_lmdb_tools.sh
-cd ~/aeon-rebase-pool/sql_sync/
+bash ~/durins-nodejs-pool/deployment/install_lmdb_tools.sh
+cd ~/durins-nodejs-pool/sql_sync/
 env PATH=$PATH:`pwd`/.nvm/versions/node/v8.9.3/bin node sql_sync.js
 echo "You're setup!  Please read the rest of the readme for the remainder of your setup and configuration.  These steps include: Setting your Fee Address, Pool Address, Global Domain, and the Mailgun setup!"
